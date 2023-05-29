56e8520d32f3f55e2648b0d500f0d4fb72edc25c
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-05-29 21:14:30 +0100

    Remove unused commit

--- components/Services/index.js
+++ components/Services/index.js
@@ -1,10 +1,5 @@
 import IconLoader from 'icons';
 
-// git branch -m main master
-// git fetch origin
-// git branch -u origin/master master
-// git remote set-head origin -a
-
 const Services = () => {
     return (
         <div className='mt-28'>
