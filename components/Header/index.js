7d9350c3219d09fa8547faba4bf0570fa6b8eed2
Ayo1984 <ayomide.bamigbade@zoro.co.uk>
2023-05-29 21:10:21 +0100

    Added header --in progress

---
+++ components/Header/index.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className='text-3xl text-center bg-zinc-900 p-3 sticky top-0'>
+            Aqua
+        </div>
+    )
+};
+
+export default Header;
